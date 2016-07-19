1fe54ca773467dca01bbfd848c71fdda2a564183
paulkenrick <pkenrick@gmail.com>
2016-07-20 00:08:20 +0100

    Completes github login functionality

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -249,6 +249,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  config.omniauth :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"],
+                  callback_url: "http://localhost:3000/users/auth/github/callback"
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
