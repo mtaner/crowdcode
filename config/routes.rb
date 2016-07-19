c0dc4d09eb0e1c638824363efdb35e997b750d29
Abdulla <abdulla.ali.mahmood@gmail.com>
2016-07-19 17:45:29 +0100

    created profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  
+
 
   root to: 'projects#index'
 
   devise_for :users
+  resources :users
 
   resources :projects, shallow: true do
 		resources :features do
