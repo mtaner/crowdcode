3bd9dd19ef666877b0c50a3a1cbf9b370a1774c7
paulkenrick <pkenrick@gmail.com>
2016-07-20 21:18:41 +0100

    Completes functionality to send email to developer when they are assigned to a feature

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@crowdcode.com'
 
-  def welcome_email(user)
+  def welcome_email(user, feature)
     @user = user
+    @feature = feature
     mail(to: @user.email, subject: 'Welcome to CrowdCode')
   end
 end
