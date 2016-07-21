99100ea804e1e1e0981c3932ca858016fb020fba
mtaner <mervetaner88@gmail.com>
2016-07-21 19:31:50 +0100

    Travis test

--- spec/features/projects_feature_spec.rb
+++ spec/features/projects_feature_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Projects' do
 
   before do
-    visit('/')
+    visit root_path
     click_link('Sign up')
     fill_in('Email', with: 'test@example.com')
     fill_in('Password', with: 'testtest')
@@ -13,21 +13,21 @@ feature 'Projects' do
 
   context 'adding a project' do
     scenario 'should display a link to add a project' do
-      visit '/projects'
-      expect(page).to have_link 'Start a project'
-      click_link 'Start a project'
-      expect(current_path).to eq '/projects/new'
+      visit projects_path
+      expect(page).to have_link('Start a project')
+      click_link('Start a project')
+      expect(current_path).to eq new_project_path
     end
   end
 
   context 'creating a project' do
     scenario 'prompt user to fill out a form, then displays the new project' do
-      visit '/projects/new'
-      fill_in 'Name', with: 'Songodoro'
-      fill_in 'Description', with: 'Music break'
-      fill_in 'Launch date', with: '01/12/2016'
-      click_button 'Create Project'
-      expect(page).to have_content 'Songodoro'
+      visit new_project_path
+      fill_in('Name', with: 'Songodoro')
+      fill_in('Description', with: 'Music break')
+      fill_in('Launch date', with: '01/12/2016')
+      click_button('Create Project')
+      expect(page).to have_content('Songodoro')
     end
   end
 end
