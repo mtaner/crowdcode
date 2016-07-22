9a713552297fb9988e33cead90ec1a195b441c34
riyapabari <riya.pabari@gmail.com>
2016-07-22 08:57:59 +0100

    completes project images and styling

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,8 @@ class Project < ApplicationRecord
 
 	belongs_to :user
 	has_many :features, dependent: :destroy
+
+	has_attached_file :image, :styles => { :large => "700x700>", :medium => "300x300>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720155903) do
+ActiveRecord::Schema.define(version: 20160721215402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,12 +26,16 @@ ActiveRecord::Schema.define(version: 20160720155903) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "name"
     t.text     "description"
     t.datetime "launch_date"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
